7591758802cd2013d6b3c9c50f7410a38f589230
TBxns <t_bexkens@hotmail.com>
2026-09-11 16:05:00 +0200

    Update public-config.js

--- supabase/public-config.js
+++ supabase/public-config.js
@@ -1,4 +1,4 @@
 window.MALLORCA_SUPABASE = {
   url: "https://admyboikbqnvysurmnde.supabase.co",
-  key: sb_publishable_KbWp3uaucFKv1kp2Jr4maA_gS2bvMcv
+  key: "sb_publishable_KbWp3uaucFKv1kp2Jr4maA_gS2bvMcv"
 };
